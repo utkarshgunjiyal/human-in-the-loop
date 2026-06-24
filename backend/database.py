61c0221e0c1305c66ce377b5b24c92ede435ac8b
utkarshgunjiyal-star <utkarshgunjiyal@gmail.com>
2026-06-24 14:40:00 +0530

    Fix MongoDB TLS certificate configuration

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,15 @@
 """Database connection and shared MongoDB client."""
 import os
+import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 
 mongo_url = os.environ["MONGO_URL"]
-client = AsyncIOMotorClient(mongo_url)
+
+client = AsyncIOMotorClient(
+    mongo_url,
+    tlsCAFile=certifi.where(),
+)
+
 db = client[os.environ["DB_NAME"]]
 
 
